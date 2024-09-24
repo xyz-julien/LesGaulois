0e3ada192875a3cd0d5fb33ab0e698db00248471
xyz-julien <1julien.dilmi@gmail.com>
2024-09-24 13:01:55 +0200

    Update Village.java

--- src/personnages/Village.java
+++ src/personnages/Village.java
@@ -2,9 +2,14 @@ package personnages;
 
 public class Village {
 	private String nom;
+	private Chef chef;
 
 	public Village(String nom) {
 		this.nom = nom;
+	}
+	
+	public void setChef(Chef chef) {
+		this.chef = chef;
 	}
 	
 	public String getNom() {
